5135be1e3c6a29e987d51ac16c8dfdd3c28f450e
Dmitry <60884905+DmitrySteblovskiy@users.noreply.github.com>
2024-09-12 02:17:41 +0300

    Update task_b.cpp

--- Lesson Materials/Lesson 2/task_b.cpp
+++ Lesson Materials/Lesson 2/task_b.cpp
@@ -1,15 +1,15 @@
 // В принципе, можно вставлять сверху ссылку на текст задачи / успешную посылку в Яндекс.Контест:
-// https://contest.yandex.ru/contest/40236/run-report/117792126/
+// https://contest.yandex.ru/contest/40236/run-report/117792182/
 #include <cmath>  // Алфавитный порядок инклюдов
 #include <iostream>
 
 int main() {  // Пробелы вокруг скобок, пропуск строк до и после объявления / реализации функций, классов и прочего
-  size_t first_cathetus, second_cathetus;  // Понятный нейминг для задачи
+  size_t first_cathetus, second_cathetus;  // Понятный нейминг + пользуемся натуральностью чисел из условия выбирая тип
   std::cin >> first_cathetus >> second_cathetus;
 
   // Вычисление гипотенузы, снова хороший нейминг + пробелы вокруг любых операторов
   double hypotenuse = std::sqrt(first_cathetus * first_cathetus + second_cathetus * second_cathetus);
 
-  std::cout << hypotenuse << '\n';   // '\n' вместо std::endl
+  std::cout << hypotenuse << '\n';  // '\n' вместо std::endl
 }
 // Пустая строка в конце любого (!!!) файла! Кстати, в PEP8 для гуманитарного Питона также.
