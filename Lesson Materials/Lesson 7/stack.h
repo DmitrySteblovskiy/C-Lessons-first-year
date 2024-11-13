65e4323c9a379e27788646b086462a83635d715f
DmitrySteblovskiy <steblovskii.dk@phystech.edu>
2024-11-13 18:53:33 +0300

    Lesson7

---
+++ Lesson Materials/Lesson 7/stack.h
@@ -0,0 +1,66 @@
+ #pragma once
+
+// Класс Stack
+template <typename T>
+class Stack {
+private:
+    // Node - Слава
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(T value, Node* nextNode = nullptr) : data(value) , next(nextNode) {}
+
+    };
+
+    Node* top_;
+    size_t size_{0};
+
+public:
+
+    // Конструктор и деструктор - Муслим
+    Stack() : top_(nullptr) {}
+
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    // operators
+    // Stack(const )
+
+// Добавление - Гарник
+    void push(const T value) {
+        ++size_;
+        Node* new_node = new Node(value, top_);
+        top_ = new_node;
+    };
+
+    // Удаление - Аня
+    void pop() {
+        if (!isEmpty()) {
+            --size_;
+            Node* temp = top_;
+            top_ = top_->next;
+            delete temp;
+        }
+        else {
+            std::cout << "Stack is empty\n";
+        }
+    };
+
+    // Подсмотреть элемент - Ани
+    T& top() {
+        if (isEmpty()) {
+            std::cout << "Stack is empty\n";
+        }
+        return top_->data;
+    };
+
+    // Проверка на пустоту стека - Саша
+    bool isEmpty() { return top_ == nullptr; }
+
+    // Вывод размера - Саша
+    size_t size() const {return size_;}
+};
+
